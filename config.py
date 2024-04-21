2df62a933c1d1fdcb74216a26638c9fae56ca00f
isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>
2024-04-21 14:20:51 +0300

    Update config.py
    Signed-off-by: isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,18 +32,18 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/isagzbyk466/Gorillmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dinimislam571kuran")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+50QL-_IgUhUzMzdk")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", true))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
