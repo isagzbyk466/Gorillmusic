cb30aae439bd78a2d3b90e31f502f23270069af0
isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>
2024-04-21 14:17:49 +0300

    Update config.py
    Signed-off-by: isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("13868676"))
+API_HASH = getenv("233beccbc439b93a234bd4a2e6de7892")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("7028234810:AAEf5vf9AVnbgcFGJ9x-tjAQp_ReRC7NzyA")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
