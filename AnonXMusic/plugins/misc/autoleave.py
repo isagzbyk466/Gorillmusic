be22343ab3cf1d00a5ec066ad88b99e8872cf80c
isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>
2024-05-24 23:57:44 +0300

    Update autoleave.py
    Signed-off-by: isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -65,7 +65,7 @@ async def auto_end():
                 try:
                     await app.send_message(
                         chat_id,
-                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+                        "» Kimse Dinlemediği İçin Bot Sohbetten Ayrıldı Haber verimde sonra noluyo lan demeyin :D",
                     )
                 except:
                     continue
