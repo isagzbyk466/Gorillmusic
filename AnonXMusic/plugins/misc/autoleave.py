94e144650860929ae0fd601dd8ba825f8599c474
isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>
2024-05-27 17:55:35 +0300

    Update autoleave.py
    Signed-off-by: isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,7 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1002068535245
                             ):
                                 if left == 20:
                                     continue
