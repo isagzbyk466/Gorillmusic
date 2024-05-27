757db72887b1130f354fe10284b7b7a3b9946db0
isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>
2024-05-27 17:43:40 +0300

    Update loop.py
    Signed-off-by: isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>

--- AnonXMusic/plugins/admins/loop.py
+++ AnonXMusic/plugins/admins/loop.py
@@ -30,13 +30,13 @@ async def admins(cli, message: Message, _, chat_id):
             )
         else:
             return await message.reply_text(_["admin_17"])
-    elif state.lower() == "enable":
+    elif state.lower() == "aktif":
         await set_loop(chat_id, 10)
         return await message.reply_text(
             text=_["admin_18"].format(state, message.from_user.mention),
             reply_markup=close_markup(_),
         )
-    elif state.lower() == "disable":
+    elif state.lower() == "devre dışı":
         await set_loop(chat_id, 0)
         return await message.reply_text(
             _["admin_19"].format(message.from_user.mention),
